cc5ab32d8501bd5ea26741ab42ab74eeefb10052
x <x>
2026-07-15 04:51:15 +0300

    almost done

--- server.mjs
+++ server.mjs
@@ -127,7 +127,8 @@ import { createServer } from "http";
 import next from "next";
 import { Server } from "socket.io";
 
-const dev = process.env.NODE_ENV !== "production";
+// const dev = process.env.NODE_ENV !== "production";
+const dev = "development";
 const hostname = "0.0.0.0";
 const port = process.env.PORT || 3000;
 
